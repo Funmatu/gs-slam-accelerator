44c3b6b191c5eb03d4e37f775bbed655cfdbc5f5
FUKUMURA Hajime <hajime.fukumura.osaka.univ@gmail.com>
2026-01-21 16:56:50 +0900

    setup: initialize project gs-slam-ccelerator

--- www/index.js
+++ www/index.js
@@ -1,4 +1,4 @@
-import init, { compute_metrics_js } from './pkg/nx_compute_rs.js';
+import init, { compute_metrics_js } from './pkg/gs_slam_ccelerator.js';
 
 async function run() {
     await init(); // Initialize WASM
